525921162d0ccc201442b256e524960ec29e52ee
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-18 22:28:50 -0300

    map, filter and reduce

--- workshop/index.js
+++ workshop/index.js
@@ -1,1 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6] // Methods of Array: map, filter, reduce - High Order Functions
+
+const double = x => x * 2
+const isEven = x => x % 2 === 0
+const add = (x, y) => x + y
+
+console.log(
+    numbers.map(double), // .map abstracts our iteration
+    numbers.filter(isEven), // .filter also abstracts the filtering
+    numbers.reduce(add, 0) // .reduce runs through an array and accumulates the results
+)
